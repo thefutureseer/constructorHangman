afd795aac9ab35e11f3345111f5270a1558158cd
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2018-01-27 20:01:44 -0800

    add pseudo coded files plus git ignore file and lock js

---
+++ Letter.js
@@ -0,0 +1,25 @@
+//This file should not require any other files.....
+
+// This file contains a constructor letter this constructor should be able to either 
+//display an underlying character or a blank placeholder (such as 
+//an underscore), depending on whether or not the user has guessed the 
+//letter. That means the constructor should define::::
+
+// A string value to store the underlying character for the letter
+// A boolean value that stores whether that letter has been guessed yet
+
+// A function that returns the underlying character if the letter has been 
+//guessed, or a placeholder (like an underscore) if the letter has not been guessed
+// A function that takes a character as an argument and checks it against
+// the underlying character, updating the stored boolean value to true if it 
+//was guessed correctly
+
+
+
+
+
+
+HINT: Write Letter.js first and test it on its own before moving on, then do the same thing with Word.js
+
+HINT: If you name your letter's display function toString, JavaScript will call that function automatically whenever casting that object to a string (check out this example: https://jsbin.com/facawetume/edit?js,console)
+
